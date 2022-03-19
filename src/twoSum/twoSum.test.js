087c780d6c4a216f1892235124e86217b41ef450
Tanvir Rahman <rahmantanvir25558@gmail.com>
2022-03-19 16:33:16 +0600

    added test coverage

---
+++ src/twoSum/twoSum.test.js
@@ -0,0 +1,21 @@
+import { twoSum } from './index'
+
+describe('twoSum', () => {
+  describe('odd number of elements', () => {
+    it('indices of the two numbers such that they add up to target', () => {
+      const inputArray = [2,7,11,15];
+      const target = 9;
+      const expectedResult = [0,1];
+      expect(twoSum(inputArray, target)).toEqual(expectedResult);
+    })
+  })
+
+  describe('even number of elements', () => {
+    it(' indices of the two numbers such that they add up to target', () => {
+      const inputArray = [2,7,11];
+      const target = 9;
+      const expectedResult = [0,1];
+      expect(twoSum(inputArray, target)).toEqual(expectedResult);
+    })
+  })
+})
